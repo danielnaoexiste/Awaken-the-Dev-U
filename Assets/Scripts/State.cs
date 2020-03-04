fea1d175c53eb622083082b606232161d55d66fa
Daniel <danieldenardo1@gmail.com>
2020-03-04 07:46:58 -0300

    yup

--- Assets/Scripts/State.cs
+++ Assets/Scripts/State.cs
@@ -1,21 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 [CreateAssetMenu(menuName = "State")]
 public class State : ScriptableObject
 {
-
+    public string type;
+    public string inputCode;
+    public string[] choices;
     [TextArea(10,14)] [SerializeField] string storyText;
     [SerializeField] State[] nextStates;
+    
 
     public string GetStateStory()
     {
         return storyText;
     }
 
+    public string GetStateType() 
+    {
+        return type;
+    }
     public State[] GetNextStates()
     {
         return nextStates;
+    }
+
+    public int GetChoiceSize() 
+    {
+        return choices.Length;
+    }
+
+    public string[] GetChoices() {
+        return choices;
+    }
+
+    public string GetInputCode() {
+        return inputCode;
     }
 }
--- Assets/Scripts/StateManager.cs
+++ Assets/Scripts/StateManager.cs
@@ -8,33 +8,82 @@ public class StateManager : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI textComponent;
     [SerializeField] State startingState;
+    [SerializeField] GameObject inputs;
+    [SerializeField] TextMeshProUGUI inputCode; 
+    [SerializeField] GameObject choices;
 
     State state;
-    // Start is called before the first frame update
-    void Start()
+    int cont = 0;
+    void Start() 
     {
         state = startingState;
-        textComponent.text = state.GetStateStory();
+        // textComponent.text = state.GetStateStory();
+        StartCoroutine(TypeText(state.GetStateStory()));
+        // ManageType();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        ManageState();
-    }
-
-    private void ManageState() 
-    {
+    public void ChangeState(int num) {
         var nextStates = state.GetNextStates();
-        for (int i = 0; i < nextStates.Length; i++) 
-        {
-            // Mudar para input mobile
-            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
-            {
-                state = nextStates[i];
+                for(int k = 3; k >= 0; k--) {
+                    choices.transform.GetChild(0).GetChild(k).transform.localScale = new Vector3(1, 1, 0);
+                }
+                state = nextStates[num];
+                choices.SetActive(false);
+                inputCode.text = "";
+                cont = 0;
+                inputs.SetActive(false);
+                StopAllCoroutines();
+                StartCoroutine(TypeText(state.GetStateStory()));
             }
-        }
 
-        textComponent.text = state.GetStateStory();
+    private void ManageType()
+    {
+        var stateType = state.GetStateType();
+        if (stateType == "Choices") {
+            choices.SetActive(true);
+            inputs.SetActive(false);
+            string[] a = state.GetChoices();
+            for(int i = 0; i < state.GetChoiceSize(); i++) {
+                choices.transform.GetChild(0).GetChild(i).GetChild(0).GetComponent<TextMeshProUGUI>().text = a[i];
+            }
+            
+            for(int i = 3; i >= state.GetChoiceSize(); i--) {
+                choices.transform.GetChild(0).GetChild(i).transform.localScale = new Vector3(0, 0, 0);
+            }
+
+        } else if (stateType == "Input") {
+            choices.SetActive(false);
+            inputs.SetActive(true);
+        }
+    }
+
+    public void AddNumberToInputCode(int num) 
+    {
+        if (cont < 4) {
+            inputCode.text += num.ToString();
+            cont++;
+            if (cont == 4) 
+            {
+                if (inputCode.text == state.GetInputCode()) 
+                {
+                    ChangeState(0);
+                } else {
+                    inputCode.text = "";
+                    cont = 0;
+                }
+            }
+        } 
+    }
+
+    IEnumerator TypeText(string textToDisplay) 
+    {
+        textComponent.text = "";
+        foreach(char letter in textToDisplay.ToCharArray()) 
+        {
+            textComponent.text += letter;
+            yield return null; 
+        }
+        ManageType();
+        yield return null;
     }
 }
