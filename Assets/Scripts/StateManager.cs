08552b4066b4bd68d8629b2bacd39fa2517ffb93
Daniel <danieldenardo1@gmail.com>
2020-03-03 23:52:50 -0300

    Initial Commit

---
+++ Assets/Scripts/StateManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class StateManager : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI textComponent;
+    [SerializeField] State startingState;
+
+    State state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState() 
+    {
+        var nextStates = state.GetNextStates();
+        for (int i = 0; i < nextStates.Length; i++) 
+        {
+            // Mudar para input mobile
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+            {
+                state = nextStates[i];
+            }
+        }
+
+        textComponent.text = state.GetStateStory();
+    }
+}
